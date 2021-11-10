c8f1cbb1092bf3f2e56c5e0ea90c8339f2d4f9ba
Plain-ST <54475309+Plain-ST@users.noreply.github.com>
2021-11-10 17:15:59 +0900

    first commit

---
+++ js/game.js
@@ -0,0 +1,57 @@
+let timer = null;
+function init() {
+  if (timer == null) {
+    start = new Date();
+    time();
+    gameStart();
+  }
+}
+
+function gameStart() {
+  let size = 5;
+  let qNum = Math.floor(Math.random()*q.length);
+  if(reset==1){
+    while(cells.firstChild){
+      cells.removeChild(cells.firstChild);
+    }
+  }
+  for(let i = 0; i<size*size; i++){
+    let s = document.createElement("span");
+    s.textContent = q[qNum][0];
+    s.setAttribute("id","num" + i);
+    s.addEventListener('click',function(){
+      if(this.textContent ==q[qNum][1]){
+        correct.play();
+        if(stage == max){
+          clearTimeout(timer);
+          alert("Game Completed!!\nYour time is "+ eTime+" seconds");
+          timer = null;
+          reset = 1;
+        }else{
+          while(cells.firstChild){
+            cells.removeChild(cells.firstChild);
+          }
+          stage++;
+          gameStart();
+        }
+      }else{
+        wrong.play();
+      }
+    });
+    cells.appendChild(s);
+    if(i % size == size -1 ){
+      const br = document.createElement("br");
+      cells.appendChild(br);
+    }
+  }
+  let p = Math.floor(Math.random()*size*size);
+  let ans = document.getElementById("num"+p);
+  ans.textContent = q[qNum][1];
+}
+
+function time() {
+  let nowTime = new Date();
+  eTime = parseInt((nowTime.getTime() - start.getTime())/1000);
+  score.textContent = eTime;
+  timer = setTimeout("time()",1000);
+}
---
+++ js/init.js
@@ -0,0 +1,21 @@
+const q = [
+  ['高', '髙'],
+  ['吉', '𠮷'],
+  ['酒', '洒'],
+  ['人', '入'],
+  ['昴', '昂'],
+  ['鳴', '嗚'],
+  ['倶', '惧'],
+  ['師', '帥'],
+  ['狭', '挟'],
+  ['壁', '璧']
+];
+const cells = document.getElementById("cells");
+const score = document.getElementById("time");
+let start = new Date();
+const correct = new Audio('sound/correct.mp3');
+const wrong = new Audio('sound/wrong.mp3');
+const max = 5;
+let stage = 0;
+let eTime;
+let reset = 0;
